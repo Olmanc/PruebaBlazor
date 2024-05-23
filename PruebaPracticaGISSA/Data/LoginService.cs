c68489a63fa43179b5b4522cc30cb67b5484fab0
Olmanc <oruman.247@gmail.com>
2024-05-23 13:20:23 -0600

    gitguardian

--- PruebaPracticaGISSA/Data/LoginService.cs
+++ PruebaPracticaGISSA/Data/LoginService.cs
@@ -27,7 +27,7 @@ namespace PruebaPracticaGISSA.Data
                 return result;
 
 
-                //_client.DefaultRequestHeaders.Add("x-api-key", "prueba_E5QvNRSiLI_N3KNebWz97_E1NzQgGuaM0P8_Q1n6sujAPfDeydsmsTee_Cw7a2f9rNcCntJLVI_yQxrQELDKJw==");
+                //_client.DefaultRequestHeaders.Add("x-api-key", "");
                 //var response = await _client.PostAsync("api/Login/GISSA", new StringContent(JsonSerializer.Serialize(new { user = user.user, pass = user.pass }), Encoding.UTF8, "application/json"));
 
 
